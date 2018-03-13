ab677cfb3f32d44ab84ab89b8510249f35001369
_codefreak <dramokhola@gmail.com>
2018-03-13 14:57:05 +0200

    Updated comments

--- media.py
+++ media.py
@@ -6,9 +6,9 @@ class Video():
         Video Class provides a way to store
         video related information
     """
-    def __init__(self, title):
+    def __init__(self, trailer_youtube_url):
         # Initialize Video Class Instance Variables
-        self.title = title
+        self.trailer_youtube_url = trailer_youtube_url
 
 
 class Movie(Video):
@@ -21,12 +21,16 @@ class Movie(Video):
 
     def __init__(self, title, movie_storyline, poster_image,
                  trailer_youtube_url):
+        """
+            Constructor method will initialize instance variables 
+            and variables inhertied from video class
+        """
         # Initialize Variables Inhertied From Video Class
-        Video.__init__(self, title)
+        Video.__init__(self, trailer_youtube_url)
         # Initialize Movie Class Instance Variables
+        self.title = title
         self.movie_storyline = movie_storyline
         self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube_url
 
     def show_trailer(self):
         # This Method Shows The Trailer Of The Instance It Is Called From
